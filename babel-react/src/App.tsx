61b4c264fdad8fe5a10fa9eb7c71b61dbb1e77b9
kazuma1989 <funifuni.1204@gmail.com>
2020-03-15 18:49:14 +0900

    chore: babel-react を追加

---
+++ babel-react/src/App.tsx
@@ -0,0 +1,10 @@
+import * as preact from "/web_modules/preact.js";
+import Title from "./Title.js";
+
+export default function App() {
+  return (
+    <div>
+      <Title label="Babel + React" />
+    </div>
+  );
+}
