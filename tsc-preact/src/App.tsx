14696fbbf67d05dccae04cc7c84189ecb69b8bfd
kazuma1989 <funifuni.1204@gmail.com>
2020-03-15 18:45:51 +0900

    feat: 画面を表示できるようにした

--- tsc-preact/src/App.tsx
+++ tsc-preact/src/App.tsx
@@ -1,7 +1,10 @@
+import * as preact from "/web_modules/preact.js";
+import Title from "./Title.js";
+
 export default function App() {
   return (
     <div>
-      <h1>tsc + Preact</h1>
+      <Title label="tsc + Preact" />
     </div>
   );
 }
---
+++ tsc-preact/src/Title.tsx
@@ -0,0 +1,5 @@
+import * as preact from "/web_modules/preact.js";
+
+export default function Title({ label }: { label?: string }) {
+  return <h1>{label}</h1>;
+}
